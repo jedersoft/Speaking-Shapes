1e82ad0d38c5732f12400dcf2df712486db4ec1e
jedersoft <105174276+jedersoft@users.noreply.github.com>
2022-05-11 00:00:03 +0200

    Squashed commit of the following:
    commit ac87a07ea74ca89ca8187b2b0fcbb1057b2081ca
    Author: jedersoft <105174276+jedersoft@users.noreply.github.com>
    Date:   Tue May 10 23:58:05 2022 +0200
    
        Bye Button
    
        Bye makes the character say goodbye, disappears and also makes the buttons disappear.
    
    commit e14a739839f4552389180a71595eafdac637d0c5
    Author: jedersoft <105174276+jedersoft@users.noreply.github.com>
    Date:   Tue May 10 23:50:09 2022 +0200
    
        Button Functionality
    
        'Hello' and 'Need something?' are now working.
    
    commit 64602a039f3b6be09c6286c26111df762dc0f99f
    Author: jedersoft <105174276+jedersoft@users.noreply.github.com>
    Date:   Tue May 10 23:47:04 2022 +0200
    
        Bug fixes
    
        Bug fixed where you could skip dialogues with the space bar and the bug where the characters don't show up properly in the character interactions
    
    commit 4d5f8608d5d3a69ccc63424ba07e64450a35e409
    Author: jedersoft <105174276+jedersoft@users.noreply.github.com>
    Date:   Tue May 10 23:34:31 2022 +0200
    
        Interaction Design
    
        The Design for the character interaction.

--- Assets/Scripts/Playground.cs
+++ Assets/Scripts/Playground.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class Playground : MonoBehaviour
 {
+    public GameObject[] buttons = new GameObject[3];
     private void Start()
     {
+        foreach(Character character in GameManager.Instance.character)
+        {
+            character.gameObject.SetActive(true);
+        }
         GameManager.Instance.character[0].transform.position = new Vector3(-5, 2, 0);
         GameManager.Instance.character[1].transform.position = new Vector3(0, 2, 0);
-        GameManager.Instance.character[1].transform.position = new Vector3(5, 2, 0);
+        GameManager.Instance.character[2].transform.position = new Vector3(5, 2, 0);
+    }
+    public void MakeLeave(int ID)
+    {
+        Character character = GameManager.Instance.character[ID];
+        character.SayGoodbye();
+        character.gameObject.SetActive(false);
+        buttons[ID].SetActive(false);
     }
 }
